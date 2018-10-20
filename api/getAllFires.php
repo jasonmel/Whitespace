7718e17927212a98cc301c662f28bd617995ffaf
Ubuntu <jasonmel@whitespace.uprmf3zwzfje5dutovoxnltoih.hx.internal.cloudapp.net>
2018-10-20 04:20:07 +0000

    a version

--- api/getAllFires.php
+++ api/getAllFires.php
@@ -4,35 +4,41 @@ header("Content-Type: application/json; charset=UTF-8");
 $fire1->id = 1;
 $fire1->title = "NTNU Fire";
 $fire1->reporter = "jasonmel";
-$fire1->date = new DateTime('2018-10-19T20:03:01.012345Z');;
+$fire1->time = new DateTime('2018-10-19T20:03:01.012345Z');;
 $fire1->lat = 25.026072;
 $fire1->lon = 121.527535;
 $fire1->description = "Help! Help! NTNU Fire!!!";
 $fire1->status = 0;
 $fire1->like = 12;
 $fire1->dislike = 1;
+$fire1->liked = false;
+$fire1->disliked = true;
 
 $fire2->id = 2;
 $fire2->title = "NTU Fire";
 $fire2->reporter = "whitety";
-$fire2->date = new DateTime('2018-10-20T08:01:02.012345Z');;
+$fire2->time = new DateTime('2018-10-20T08:01:02.012345Z');;
 $fire2->lat = 25.016844;
 $fire2->lon = 121.539710;
 $fire2->description = "Who lets the dogs out?";
 $fire2->status = 1;
 $fire2->like = 24;
 $fire2->dislike = 3;
+$fire2->liked = false;
+$fire2->disliked = false;
 
 $fire3->id = 3;
 $fire3->title = "Daan Forest Park Fire";
 $fire3->reporter = "whitets";
-$fire3->date = new DateTime('2018-10-21T09:13:19.012345Z');;
+$fire3->time = new DateTime('2018-10-21T09:13:19.012345Z');;
 $fire3->lat = 25.031807;
 $fire3->lon = 121.535939;
 $fire3->description = "Mom, I'm here... .__.\~/";
 $fire3->status = 2;
 $fire3->like = 101;
 $fire3->dislike = 5;
+$fire2->liked = true;
+$fire2->disliked = false;
 
 $myArr = array($fire1, $fire2, $fire3);
 
--- fire.php
+++ fire.php
@@ -5,9 +5,64 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Spot That Fire - Whitespace</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<style>
+.header {
+  font-weight: bold;
+}
+.body {
+}
+.footer {
+  clear: both;
+  font-size: 80%;
+  color: gray;
+}
+.footer .left {
+  float: left;
+}
+.footer .right {
+  float: right;
+  text-align: right;
+}
+.footer .name {
+  font-weight: bold;
+}
+.footer .time {
+  font-style: italic;
+}
+.status {
+  font-weight: normal;
+  font-size: 80%;
+  padding: 3px 6px;
+  border-radius: 3px;
+  color: white;
+}
+.status.onfire {
+  background: IndianRed;
+}
+.status.extinguishing {
+  background: Coral;
+}
+.status.extinguished {
+  background: SeaGreen;
+}
+.liked {
+  font-weight: bold;
+  padding: 3px 6px;
+  border-radius: 3px;
+  color: white;
+  background: MediumSeaGreen;
+}
+.disliked {
+  font-weight: bold;
+  padding: 3px 6px;
+  border-radius: 3px;
+  color: white;
+  background: LightCoral;
+}
+</style>
 <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-3.3.1.js"></script>
 <!--
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/abs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 -->
 <script type="text/javascript" src="https://www.bing.com/api/maps/mapcontrol?key=AlkQSf2WwyWqmfhNff-HT_ZJ23yrjexI7SzU0pZMoezJdoXRB8gGy4Hh8PLrgijP"></script>
@@ -15,6 +70,52 @@
 var map;
 var searchManager;
 var mapMode = true;
+var centerPushpin;
+var pushpins = [];
+var infoboxes = [];
+
+function getStatusEmoji(status) {
+  var emoji = "&#x1F525;"; // Fire - https://emojipedia.org/fire/
+  if (status == 1) {
+    emoji = "&#x1F692;"; // Fire Engine - https://emojipedia.org/fire-engine/
+  } else if (status == 2) {
+    emoji = "&#x1F4A7;"; // Droplet - https://emojipedia.org/droplet/
+  }
+
+  return emoji;
+}
+
+function getStatusTag(status) {
+  var tag = '<span class="status onfire">On Fire</span>';
+  if (status == 1) {
+    tag = '<span class="status extinguishing">Extinguishing</span>';
+  } else if (status == 2) {
+    tag = '<span class="status extinguished">Extinguished</span>';
+  }
+
+  return tag;
+}
+
+function getFeedback(fire, id) {
+  var likedClass = (fire.liked) ? ' class="liked"' : '';
+  var dislikedClass = (fire.disliked) ? ' class="disliked"' : '';
+  var feedback = '<span' + likedClass + '>&#x1F44D; ' + fire.like + '</span> &nbsp; <span' + dislikedClass + '>&#x1F44E; ' + fire.dislike + '</span>';
+
+  return feedback;
+}
+
+function getName(fire) {
+  var name = '<span class="name">' + fire.reporter + '</span>';
+
+  return name;
+}
+
+function getTime(fire) {
+  var timeStr = fire.time.date;
+  var time = '<time class="time">' + timeStr.substring(0, timeStr.lastIndexOf(":")) + '</time>';
+
+  return time;
+}
 
 function loadMapScenario() {
   map = new Microsoft.Maps.Map(document.getElementById("myMap"), {});
@@ -36,31 +137,82 @@ function loadMapScenario() {
   function selectedSuggestion(suggestionResult) {
   }
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var p = new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude);
-      setP(p);
-    });
-  } else {
-    var p = new Microsoft.Maps.Location(51.50632, -0.12714);
-    setP(p);
-  }
-  function setP(p) {
-    var pushpin = new Microsoft.Maps.Pushpin(p, { icon: '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"><text x="0" y="30" style="font-size: 30px">&#x1F525;</text></svg>', title: "Title", subTitle: "Subtitle" });
+  // get all fires
+  $("#myListUl").empty();
+  $.getJSON( "api/getAllFires.php", function( data ) {
+    for (var i = 0; i < data.length; i++) {
+      var fire = data[i];
+      addFireOnMap(fire, i);
+      addFireOnList(fire, i);
+    }
+  });
+  function addFireOnMap(fire, i) {
+    var statusEmoji = getStatusEmoji(fire.status);
+    var statusTag = getStatusTag(fire.status);
+    var feedback = getFeedback(fire, i);
+    var name = getName(fire);
+    var time = getTime(fire);
+
+    var p = new Microsoft.Maps.Location(fire.lat, fire.lon);
+    var pushpin = new Microsoft.Maps.Pushpin(p, { icon: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"><text x="0" y="28" style="font-size: 30px">' + statusEmoji + '</text></svg>', title: fire.title, subTitle: null });
     map.entities.push(pushpin);
+    pushpins.push(pushpin);
   
-    var infobox = new Microsoft.Maps.Infobox(p, { title: "Infobox",
-      description: "London", visible: false });
+    var infobox = new Microsoft.Maps.Infobox(p, { title: fire.title,
+      description: '<div clsas="header">' + statusTag + '</div><hr><div class="body">' + fire.description + '</div><hr><div class="footer"><div class="left">' + feedback + '</div><div class="right">' + name + '<br>' + time + '</div></div>',
+      maxWidth: 600,
+      maxHeight: 900,
+      visible: false });
     infobox.setMap(map);
+    infoboxes.push(infobox);
   
     Microsoft.Maps.Events.addHandler(pushpin, "click", function () {
+      for (var j = 0; j < infoboxes.length; j++) {
+        infoboxes[j].setOptions({ visible: false });
+      }
       infobox.setOptions({ visible: true });
     });
-  
-    map.setView({
-      center: p
+  }
+  function addFireOnList(fire, i) {
+    var statusEmoji = getStatusEmoji(fire.status);
+    var statusTag = getStatusTag(fire.status);
+    var feedback = getFeedback(fire, i);
+    var name = getName(fire);
+    var time = getTime(fire);
+
+    var li = $('<a href="#" class="list-group-item list-group-item-action"></a>');
+    li.append($('<div class="header">' + statusEmoji + ' ' + fire.title + '</div><div class="body" style="padding: 5px 0 12px 0;">' + fire.description + '</div><div class="footer"><div class="left">' + feedback + '</div><div class="right">' + name + ' · ' + time + '</div></div>'));
+    li.click(function(e) {
+      $("#option1").click();
+      Microsoft.Maps.Events.invoke(pushpins[i], 'click', { target: pushpins[i] });
+    });
+    $("#myListUl").append(li);
+  }
+
+  // get user location and center map on that
+  var p = new Microsoft.Maps.Location(25.026249, 121.527511);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      p = new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude);
     });
   }
+  map.setView({
+    center: p
+  });
+
+  // put center pushpin
+  Microsoft.Maps.Events.addHandler(map, 'viewchangeend', function (e) {
+    map.entities.remove(centerPushpin);
+    centerPushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"><text x="0" y="28" style="font-size: 30px">&#8853;</text></svg>',
+      anchor: new Microsoft.Maps.Point(16, 16),
+      title: "Report This Point",
+      subTitle: null });
+    map.entities.push(centerPushpin);
+
+    Microsoft.Maps.Events.addHandler(centerPushpin, "click", function () {
+      $("#option2").click();
+    });
+  });
 }
 
 $(document).ready(function() {
@@ -77,7 +229,6 @@ $(document).ready(function() {
   });
 
   $("#option1").click(function(e) {
-    console.log("#option1 clicked");
     if (mapMode) {
       $("#listContainer").show();
       $("#mapContainer").hide();
@@ -89,16 +240,27 @@ $(document).ready(function() {
   });
 
   $("#option2").click(function(e) {
-    console.log("#option2 clicked");
     if (!mapMode) {
       $("#option1").click();
     }
 
-    $("#myModal").show();
+    $("#reportModal").show();
   });
 
-  $("#closeModal, #closeButton").click(function(e) {
-    $("#myModal").hide();
+  $("#option3").click(function(e) {
+    if (!mapMode) {
+      $("#option1").click();
+    }
+
+    $("#idModal").show();
+  });
+
+  $("#reportCloseButton").click(function(e) {
+    $("#reportModal").hide();
+  });
+
+  $("#idCloseButton").click(function(e) {
+    $("#idModal").hide();
   });
 });
 </script>
@@ -114,21 +276,55 @@ $(document).ready(function() {
     </div>
   </div>
 
-  <div id="myModal" class="modal" tabindex="-1" role="dialog">
+  <div id="reportModal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
-          <button id="closeModal" type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <h5 class="modal-title">New Fire Event</h5>
+          <button id="reportCloseButton" type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          <p>Modal body text goes here.</p>
+          <form>
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input type="text" class="form-control" id="title" name="title" maxlength="30" placeholder="Fire Title...">
+              <small id="titleHelp" class="form-text text-muted">No more than 30 characters...</small>
+            </div>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <textarea class="form-control" id="description" name="description" rows="3" placeholder="Fire Description...."></textarea>
+            </div>
+          </form>
         </div>
         <div class="modal-footer">
           <button id="submitButton" type="button" class="btn btn-primary">Submit</button>
-          <button id="closeButton" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="idModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Setup My ID</h5>
+          <button id="idCloseButton" type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group">
+              <label for="id">My ID</label>
+              <input type="text" class="form-control" id="id" name="id" maxlength="20" placeholder="My ID...">
+              <small id="idHelp" class="form-text text-muted">No more than 20 characters...</small>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button id="submitButton" type="button" class="btn btn-primary">Submit</button>
         </div>
       </div>
     </div>
@@ -140,28 +336,13 @@ $(document).ready(function() {
 </div>
 <div id="listContainer" style="display: none;">
   <div id="myList" style="width: 100vw; height: 100vh; max-height: 100vh; overflow: scroll;">
-    <ul class="list-group">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Morbi leo risus</li>
-      <li class="list-group-item">Porta ac consectetur ac</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Morbi leo risus</li>
-      <li class="list-group-item">Porta ac consectetur ac</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Morbi leo risus</li>
-      <li class="list-group-item">Porta ac consectetur ac</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-      <li class="list-group-item">Morbi leo risus</li>
-      <li class="list-group-item">Porta ac consectetur ac</li>
-      <li class="list-group-item">Vestibulum at eros</li>
-    </ul>
+    <div id="myListUl" class="list-group">
+      <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
+      <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+      <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+      <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+      <a href="#" class="list-group-item list-group-item-action">Vestibulum at eros</a>
+    </div>
   </div>
 </div>
 <div id="menuContainer" class="text-center" style="width: 100vw; position: absolute; left: 0; bottom: 10px; z-index: 999;">
@@ -173,7 +354,7 @@ $(document).ready(function() {
     <input type="radio" name="options" id="option2" autocomplete="off">Report
   </label>
   <label class="btn btn-secondary">
-    <input type="radio" name="options" id="option3" autocomplete="off">Button
+    <input type="radio" name="options" id="option3" autocomplete="off">My ID
   </label>
   </div>
 </div>
